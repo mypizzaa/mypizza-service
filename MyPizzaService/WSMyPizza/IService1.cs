88601535fbe16bd996a24dff89a69063c8172e69
Miki997 <miquelgomezv@gmail.com>
2018-05-09 11:44:49 +0200

    web service

---
+++ MyPizzaService/WSMyPizza/IService1.cs
@@ -0,0 +1,49 @@
+﻿using Modelo;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WSMyPizza
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IWSLogin
+    {
+        [OperationContract]
+        Usuario LoginUsuario(string correo, string password);
+
+        [OperationContract]
+        bool addUsuario(Usuario u);
+        [OperationContract]
+
+        bool modificarUsuario(Usuario u);
+
+        [OperationContract]
+        bool eliminarUsuario(Usuario u);
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ MyPizzaService/WSMyPizza/Service1.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Modelo;
+
+namespace WSMyPizza
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WSLogin : IWSLogin
+    {
+
+        private LoginDAO instance = null;    
+        private WSLogin() { }
+        public WSLogin GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new LoginDAO();
+            }
+
+            return instance;
+        }
+        public WSLogin() {
+
+        }
+
+        public bool addUsuario(Usuario u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool eliminarUsuario(Usuario u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario LoginUsuario(string correo, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool modificarUsuario(Usuario u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
